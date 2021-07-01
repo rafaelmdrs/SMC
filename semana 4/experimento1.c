8d296fa24cc07c3c3b13312feb3170b6c06f2b0a
rafaelmdrs <rafaelmedeiros214@gmail.com>
2021-07-01 15:54:31 -0300

    Update experimento1.c

--- semana 4/experimento1.c
+++ semana 4/experimento1.c
@@ -10,26 +10,26 @@ void main(void) {
    
             while(1) {
                 
-                PORTDbits.RD0 = 1;
-                PORTDbits.RD7 = 1;
+                PORTDbits.RD0 = 1;  
+                PORTDbits.RD7 = 1;   // acende os pinos 0 e 7
                 __delay_ms(3000);
                 
-                PORTDbits.RD0 = 0;
-                PORTDbits.RD1 = 1;
+                PORTDbits.RD0 = 0;   // apaga o pinos 0 
+                PORTDbits.RD1 = 1;   // acende o 1
                 __delay_ms(1000);
 
                 PORTDbits.RD1 = 0;
-                PORTDbits.RD7 = 0;
+                PORTDbits.RD7 = 0;    // apaga os pinos 1 e 7
                 PORTDbits.RD2 = 1;
-                PORTDbits.RD5 = 1;
+                PORTDbits.RD5 = 1;    // acende os pinos 2 e 5
                 __delay_ms(3000);
                 
-                PORTDbits.RD5 = 0;
-                PORTDbits.RD6 = 1;
+                PORTDbits.RD5 = 0;    // apaga o pino 5
+                PORTDbits.RD6 = 1;    // acende o pino 6
                 __delay_ms(1000);
                 
-                PORTDbits.RD6 = 0;
-                PORTDbits.RD2 = 0;
+                PORTDbits.RD6 = 0;    
+                PORTDbits.RD2 = 0;    // apaga os pinos 6 e 2
 
             }
     
